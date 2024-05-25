06910dc8ed24504ac67974d55d88ca1230a6c015
rrupom <rakib.t.rupom@gmail.com>
2024-05-25 17:09:17 +0600

    redux thunk middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,24 +1,17 @@
 const { createStore, applyMiddleware } = require("redux")
-const { TODOADDED, TODOLOADED } = require("./async-demo/actionTypes")
-const { todoAdded, todoLoaded, fetchTodo } = require("./async-demo/actions");
 
-const { delayActionMiddleware, fetchAsyncMiddleware } = require("./middlewares/todoMiddlewares");
 const { fetchTodos } = require("./thunkFunctions");
+const thunk = require("redux-thunk");
 
 
 const reducer = require("./async-demo/reducer")
 
-const store = createStore(reducer, applyMiddleware(delayActionMiddleware, fetchAsyncMiddleware));
+//! Download redux-thunk@2.4.1 for smooth experience
+const store = createStore(reducer, applyMiddleware(thunk.default));
 
 // subscribe to the store
 store.subscribe(() => {
     console.log(store.getState());
 })
 
-//? dispatching action
-// store.dispatch(todoAdded("Learn NodeJS"));
-
-/**
- * ? If we want to load todos from a server, we need to dispatch a fake action. then after loading the data in middleware then we will dispatch actual action from middleware
- */
 store.dispatch(fetchTodos);
